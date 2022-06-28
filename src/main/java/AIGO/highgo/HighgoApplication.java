bf39fb0be3b6f288d4d5669593f5d302e651d51e
suddiyo <suddii01@sju.ac.kr>
2022-06-29 02:28:33 +0900

    initialize

---
+++ src/main/java/AIGO/highgo/HighgoApplication.java
@@ -0,0 +1,13 @@
+package AIGO.highgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AIGO/highgo/HighgoApplicationTests.java
@@ -0,0 +1,13 @@
+package AIGO.highgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
