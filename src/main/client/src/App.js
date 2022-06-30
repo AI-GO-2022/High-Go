dcadd8d8fe3c4a5604a1709215e1250ff2a5ae54
희호 <heraok0153@gmail.com>
2022-06-30 15:39:28 +0900

    mainpage background
    메인페이지 바 밑부분 제작

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -2,7 +2,7 @@
 
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-
+import './mainpage.css';
 function App() {
   const [hello, setHello] = useState('')
 
@@ -11,12 +11,16 @@ function App() {
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
   }, []);
-
+ 
   return (
       <div>
         백엔드에서 가져온 데이터입니다 : {hello}
+        <section>
+          <img className="bird" src="https://i.imgur.com/UoNlFlC.png"/>
+        </section>
       </div>
   );
+  
 }
 
 export default App;
