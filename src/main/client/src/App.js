883785d38a7f37e9973d73814d6c274d55fe8cb5
희호 <heraok0153@gmail.com>
2022-07-10 23:11:02 +0900

    메인 페이지 수정(구름 추가)

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import './components/nav.css';
 import './components/mainpage.css';
 import imagemain from './components/images/mainlogo2.png';
+import imagemain2 from './components/images/cloud.png';
+import imagemain3 from './components/images/onecloud.png';
 
 function App() {
   const [hello, setHello] = useState('')
@@ -17,7 +19,6 @@ function App() {
   return (
       <div>
         <section>
-          <img className="cloud"/>
         {hello}
 
         <div className="subtitle">LET'S GO HIGH-!</div>
@@ -29,19 +30,23 @@ function App() {
           <li><a href="#">SIGN UP</a></li>
           </ul>
         </nav>
-        <div className="title"><img className="mainlogo" src={imagemain}/></div>
+        <img className="onecloud" src={imagemain3}/>
+        <div className="title"><img className="mainlogo" src={imagemain}/>
+        </div>
         <div className="des">
           안녕하세요! HIGH-GO를 찾아주신 여러분!<br/>
           &nbsp;&nbsp;HIGH-GO는 초심자부터 전문가까지 숙련도에 따른 등산코스와 <br/>
           주변 맛집 및 관광지에 대한 정보를 제공합니다. 
           <br/>또한, 내가 정상에 오른 산의 발자취를 기록하고 
-          <br/>카카오 친구들과 공유하며 산행의 즐거움을 나눌 수 있습니다.
+          <br/>카카오 친구들과 공유하며 산행의 즐거움을 나눌 수 있습니다.        
           <br/>모두 산 정상을 향해 GO-HIGH!
         </div>
         <br/>
         <button className="recommendation">RECOMMENDATION FOR YOU!<br/>
         CLICK ME!</button>
+        <img className="cloud" src={imagemain2}/>
         </section>
+        
     </div>
   );
 }
