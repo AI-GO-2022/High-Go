d2e5601cc00a89a5ca810029686617c14a2a26c1
suddiyo <suddii01@sju.ac.kr>
2022-07-12 19:51:55 +0900

    update main page

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -7,19 +7,9 @@ import imagemain from './components/images/mainlogo2.png';
 import imagemain2 from './components/images/cloud.png';
 import imagemain3 from './components/images/onecloud.png';
 
-function App() {
-  const [hello, setHello] = useState('')
-
-  useEffect(() => {
-    axios.get('/api/hello') // get data from backend
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-  }, []);
-
   return (
       <div>
         <section>
-        {hello}
 
         <div className="subtitle">LET'S GO HIGH-!</div>
         <nav id="menubar">
@@ -37,8 +27,8 @@ function App() {
           안녕하세요! HIGH-GO를 찾아주신 여러분!<br/>
           &nbsp;&nbsp;HIGH-GO는 초심자부터 전문가까지 숙련도에 따른 등산코스와 <br/>
           주변 맛집 및 관광지에 대한 정보를 제공합니다. 
-          <br/>또한, 내가 정상에 오른 산의 발자취를 기록하고 
-          <br/>카카오 친구들과 공유하며 산행의 즐거움을 나눌 수 있습니다.        
+          <br/>또한, 내가 정상에 오른 산에 발자취를 남기고
+          <br/>산행 일지를 기록하며 산행의 즐거움을 나눌 수 있습니다.        
           <br/>모두 산 정상을 향해 GO-HIGH!
         </div>
         <br/>
